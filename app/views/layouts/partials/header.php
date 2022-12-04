210205653c174e3b7367f226a8fb09dfc12ef9fe
Newbiecodeweaver <carlosgilino@gmail.com>
2022-12-04 16:43:08 +0100

    view responsiveness improved

--- app/views/layouts/partials/header.php
+++
@@ -1,8 +0,0 @@
-<header class="mb-10">
-  <div class="columns-2 bg-gradient-to-r from-cyan-500 to-blue-500 p-10">
-    <div class="p-1"><a href="../web/"><img src="images/logo-task.png"></a></div>
-    <div class="p-6">
-      <div class="float-right w-33 rounded-lg bg-orange-400 p-4 text-center text-base font-semibold text-red-50 hover:bg-orange-500"><a href="../web/add"> <i class="fa-solid fa-circle-plus"></i></i><span class="pl-2">NUEVA TAREA</span></a></div>
-    </div>
-  </div>
-</header>
